f8c69b6c96787b6ae954efc404fc0aa5c15ef894
Enes Çiçek <bmenescicek@hotmail.com>
2024-07-07 15:45:17 +0300

    Layout completed.

---
+++ RealEstate_Dapper_UI/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    /// <summary>
+    /// HomePage
+    /// </summary>
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RealEstate_Dapper_UI/ViewComponents/Layout/_HeaderViewComponentPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents.Layout
+{
+    /// <summary>
+    /// Partial View gibi görev görmektedir.
+    /// </summary>
+    public class _HeaderViewComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
